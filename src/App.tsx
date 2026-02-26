0e7df4f6cf284c74e34004c0c4dfe59d9a34a4e2
paulo.paiva <ppaivank@gmail.com>
2026-02-25 23:39:58 -0300

    feat: add 404 page and register route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { LoginPage } from "./pages/LoginPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 import { ProfilePage } from "./pages/ProfilePage";
+import { RegisterPage } from "./pages/RegisterPage";
 import "./App.css";
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route
           path="/profile"
           element={
@@ -18,7 +21,7 @@ function App() {
           }
         />
         <Route path="/" element={<Navigate to="/profile" replace />} />
-        <Route path="*" element={<Navigate to="/profile" replace />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,8 @@
+export function RegisterPage() {
+  return (
+    <main>
+      <h1>Registrarse</h1>
+      <p>Formulario de registro (próxima fase).</p>
+    </main>
+  );
+}
